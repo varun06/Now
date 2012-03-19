783c0c4c72ef1b1291871cdb999279b8212fc7f3
Varun Kumar <varunkumar.saini06@gmail.com>
2012-03-19 07:27:46 -0500

    commit

--- gen/com/var/learn/now/R.java
+++ gen/com/var/learn/now/R.java
@@ -12,11 +12,26 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int lunchlist=0x7f020001;
+    }
+    public static final class id {
+        public static final int addr=0x7f050001;
+        public static final int devivery=0x7f050007;
+        public static final int name=0x7f050000;
+        public static final int save=0x7f050008;
+        public static final int sit_down=0x7f050006;
+        public static final int tableRow1=0x7f050002;
+        public static final int take_out=0x7f050005;
+        public static final int textView1=0x7f050003;
+        public static final int types=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int Address=0x7f040003;
+        public static final int Name=0x7f040002;
+        public static final int Save=0x7f040004;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
     }
--- src/com/var/learn/now/Now.java
+++ src/com/var/learn/now/Now.java
@@ -1,13 +1,56 @@
 package com.var.learn.now;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.LayoutParams;
+import android.widget.TableLayout;
 
 public class Now extends Activity {
+	List<Restaurant> model = new ArrayList<Restaurant>(); // create a new instance of class
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button save = (Button)findViewById(R.id.save);
+        
+        save.setOnClickListener(onSave);
     }
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			
+			Restaurant r = new Restaurant();
+			EditText name = (EditText)findViewById(R.id.name);
+			EditText address = (EditText)findViewById(R.id.addr);
+			
+			r.setName(name.getText().toString());
+			r.setAddress(address.getText().toString());
+			
+			RadioGroup types = (RadioGroup)findViewById(R.id.types);
+			
+			switch (types.getCheckedRadioButtonId()){
+				case R.id.sit_down:
+					r.setType("sit_down");
+					break;
+				case R.id.take_out:
+					r.setType("take_out");
+					break;
+				case R.id.devivery:
+					r.setType("delivery");
+					break;
+			}
+			
+		}
+	};
 }
